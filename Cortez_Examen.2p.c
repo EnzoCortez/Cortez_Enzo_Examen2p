9958062084cd15d1be4c8c5820284e2a6c48d7e0
Enzo Cortez <enzo.cortez@udla.edu.ec>
2023-12-12 15:28:54 -0500

    Correccion

--- Cortez_Examen.2p.c
+++ Cortez_Examen.2p.c
@@ -19,3 +19,30 @@ void ingresos(int matriz[MESES][FILAS][COLUMNAS]) {
     }
 }
 
+// Función para generar valores aleatorios en la matriz de egresos
+void egresos(int matriz[MESES][FILAS][COLUMNAS]) {
+    srand(time(NULL)); // Semilla para los valores aleatorios
+
+    for (int mes = 0; mes < MESES; mes++) {
+        for (int fila = 0; fila < FILAS; fila++) {
+            for (int columna = 0; columna < COLUMNAS; columna++) {
+                matriz[mes][fila][columna] = rand() % 50; // Valores aleatorios entre 0 y 49
+            }
+        }
+    }
+}
+
+// Función para sumar todos los valores de la matriz de ingresos en una matriz única
+void sumarIngresos(int matrizIngresos[MESES][FILAS][COLUMNAS], int matrizTotal[FILAS][COLUMNAS]) {
+    for (int fila = 0; fila < FILAS; fila++) {
+        for (int columna = 0; columna < COLUMNAS; columna++) {
+            matrizTotal[fila][columna] = 0; // Inicializar la matriz total en 0
+            for (int mes = 0; mes < MESES; mes++) {
+                for (int i = 0; i < COLUMNAS; i++) {
+                    matrizTotal[fila][i] += matrizIngresos[mes][fila][i];
+                }
+            }
+        }
+    }
+}
+
